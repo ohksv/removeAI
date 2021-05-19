e090d12047074ecd29100ec24ad8a8253e00e96e
kura <72899137+x6k@users.noreply.github.com>
2021-05-19 00:48:51 -0400

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,23 @@
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0) -- Waits Every Tick
+
+        -- Managing the Traffic, as well as Ped Density. (BOTH MUST RUN EVERY TICK)
+        SetTrafficDensity(0.0) -- (Vehicle Density) 0.0 is none, and 1.0 is the max.
+        SetPedDensity(0.0) -- (Ped Density) 0.0 is none, and 1.0 is the max.
+    end
+end)
+
+-- IGNORE, DO NOT EDIT
+-- Vehicle Density
+function SetTrafficDensity(density)
+    SetParkedVehicleDensityMultiplierThisFrame(density)
+    SetVehicleDensityMultiplierThisFrame(density)
+    SetRandomVehicleDensityMultiplierThisFrame(density)
+end
+
+-- Ped Density
+function SetPedDensity(density)
+    SetPedDensityMultiplierThisFrame(density)
+    SetScenarioPedDensityMultiplierThisFrame(density)
+end
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Kura'
+description 'Removes/Reduces AI'
+version '1.0.0'
+
+client_script 'client.lua'
